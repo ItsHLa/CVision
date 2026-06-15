ea6489506ca1de478fd21baefb89f17a1a2b7c31
ItsHLa <nissanihala@gmail.com>
2026-06-15 19:52:26 +0300

    set redis URL as env

--- server/services/redis_service.py
+++ server/services/redis_service.py
@@ -1,7 +1,8 @@
 import json
 from redis import Redis
 from redis.asyncio import Redis as AsyncRedis
+import os
 
-REDIS_URL = "rediss://default:gQAAAAAAAd-wAAIgcDI0N2M5YWVlN2YwMWI0ZjQyOTI4NjRmNGRlYTg4MzcwOA@cute-gull-122800.upstash.io:6379?ssl_cert_reqs=none"
+REDIS_URL = os.environ.get("REDIS_URL")
 redis_client = Redis.from_url(REDIS_URL, decode_responses=True)
 async_redis_client = AsyncRedis.from_url(REDIS_URL, decode_responses=True)
